22f8b8dd8615c1b9343782f041ca801ec3f6583b
Arth Mishra <arthmishra1504@gmail.com>
2024-04-26 01:09:51 +0530

    Testing n+1st time

--- src/pages/contestant_information/index.js
+++ src/pages/contestant_information/index.js
@@ -2,17 +2,24 @@ import { Button, Divider } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import React, { useState } from 'react'
 import Contestant_information from 'src/views/form-layouts/Contestant_information'
-import BaseTable from 'src/views/tables/BaseTable';
+import BaseTable from 'src/views/tables/BaseTable'
 
 const Index = () => {
-  const [constestantList, setContestantList] = useState(false);
+  const [constestantList, setContestantList] = useState(false)
+
   return (
-  <div>
-      <Grid item xs={12} sx={{textAlign:"center"}}>
-        <Button onClick={()=>setContestantList(prev=>!prev)} sx={{marginBottom:"10px"}}  type='button' variant='contained' size='medium'>
-          {!constestantList?"Show Contestant List":"Hide Contestant List"}
+    <div>
+      <Grid item xs={12} sx={{ textAlign: 'center' }}>
+        <Button
+          onClick={() => setContestantList(prev => !prev)}
+          sx={{ marginBottom: '10px' }}
+          type='button'
+          variant='contained'
+          size='medium'
+        >
+          {!constestantList ? 'Show Contestant List' : 'Hide Contestant List'}
         </Button>
-        {constestantList?<BaseTable/>:""}
+        {constestantList ? <BaseTable /> : ''}
       </Grid>
       <Divider sx={{ my: 5 }}>Add Contestant</Divider>
       <Contestant_information />
