80aab3672e220df0bb5aeaad4b4a1519f228bfa2
Arth Mishra <arthmishra1504@gmail.com>
2024-04-26 01:05:14 +0530

    Testing nth time

--- src/pages/contestant_information/index.js
+++ src/pages/contestant_information/index.js
@@ -2,12 +2,12 @@ import { Button, Divider } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import React, { useState } from 'react'
 import Contestant_information from 'src/views/form-layouts/Contestant_information'
-import Update_Contestant from 'src/views/form-layouts/Update_Contestant'
 import BaseTable from 'src/views/tables/BaseTable';
 
 const Index = () => {
   const [constestantList, setContestantList] = useState(false);
-  return <div>
+  return (
+  <div>
       <Grid item xs={12} sx={{textAlign:"center"}}>
         <Button onClick={()=>setContestantList(prev=>!prev)} sx={{marginBottom:"10px"}}  type='button' variant='contained' size='medium'>
           {!constestantList?"Show Contestant List":"Hide Contestant List"}
@@ -17,6 +17,7 @@ const Index = () => {
       <Divider sx={{ my: 5 }}>Add Contestant</Divider>
       <Contestant_information />
     </div>
+  );
 }
 
 export default Index
